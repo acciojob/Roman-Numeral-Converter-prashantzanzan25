425e7c86aad8da572621f768e8e1da4a3b7d0030
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-04-09 12:10:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,7 +10,16 @@ function convertToRoman(num) {
     };
 
   //your code here
+	let result = '';
 
+  for (let key in obj) {
+    while (num >= obj[key][1]) {
+      result += obj[key][0];
+      num -= obj[key][1];
+    }
+  }
+
+  return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
